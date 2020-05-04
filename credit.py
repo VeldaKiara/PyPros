3a9c21aa19391da318ad9bc43ce00653794ecd64
Velda Kiara <veldakarimi@gmail.com>
2020-05-05 02:58:56 +0300

    addition of elif statements

--- credit.py
+++ credit.py
@@ -17,3 +17,17 @@ def graduation_reqs(gpa, credits):
     #If they do not have enough credits and their GPA is less than 2.0, the function should return
   if not (gpa >= 2.0) and not (credits >= 120):
     return "You do not meet either requirement to graduate!"
+
+
+
+#elif statements
+def graduation_reqs(gpa, credits):
+      if (gpa >= 2.0) and (credits >= 120):
+        return "You meet the requirements to graduate!"
+      if (gpa >= 2.0) and not (credits >= 120):
+        return "You do not have enough credits to graduate."
+      if not (gpa >= 2.0) and (credits >= 120):
+        return "Your GPA is not high enough to graduate."
+      elif (gpa < 2.0) and (credits < 120):
+        return "You do not meet the GPA or the credit requirement for graduation."
+    
