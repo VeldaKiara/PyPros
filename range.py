d6a1e32fd6e69257863fbd7aa1db4a5fd81fb1ec
Velda Kiara <veldakarimi@gmail.com>
2020-05-10 03:54:23 +0300

    changes

--- range.py
+++ range.py
@@ -4,7 +4,6 @@ def in_range(num, lower, upper):
     return True
   else:
     return False
-# Uncomment these function calls to test your in_range function:
 print(in_range(10, 10, 10))
 # should print True
 print(in_range(5, 10, 20))
