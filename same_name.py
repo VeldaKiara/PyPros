b2974c4467f7bd445ed86de6b129710253f75e7d
Velda Kiara <veldakarimi@gmail.com>
2020-05-10 03:40:17 +0300

    added same name code

---
+++ same_name.py
@@ -0,0 +1,11 @@
+# Write your same_name function here:
+def same_name(your_name, my_name):
+  if your_name == my_name:
+    return True
+  else:
+    return False
+# Uncomment these function calls to test your same_name function:
+print(same_name("Velda", "Velda"))
+# should print True
+print(same_name("Velda", "Karimi"))
+# should print False
