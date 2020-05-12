0913b31ee0bab0b9a7e25bdc5f3a6015e8926cac
Velda Kiara <veldakarimi@gmail.com>
2020-05-12 05:06:34 +0300

    addition of items

---
+++ plus.py
@@ -0,0 +1,9 @@
+orders = ['daisy', 'buttercup', 'snapdragon', 'gardenia', 'lily']
+
+# Create new orders here:
+new_orders = orders + ['lilac', 'iris']
+print(new_orders)
+
+
+broken_prices = [5, 3, 4, 5, 4] + [4]
+print(broken_prices)
