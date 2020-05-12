97624247739763d27f03545b827e4621f29d27ba
Velda Kiara <veldakarimi@gmail.com>
2020-05-12 04:56:12 +0300

    added zip

---
+++ zip.py
@@ -0,0 +1,8 @@
+names = ['Jenny', 'Alexus', 'Sam', 'Grace']
+dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
+#zip takes two (or more) lists as inputs and returns an object that contains a list of pairs...
+names = ['Jenny', 'Alexus', 'Sam', 'Grace']
+dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
+names_and_dogs_names = zip(names, dogs_names)
+list_of_names_and_dogs_names = list(names_and_dogs_names)
+print(list_of_names_and_dogs_names)
