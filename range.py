ebe46a9ace70c0336cc794ce01641675faea1aab
Velda Kiara <veldakarimi@gmail.com>
2020-05-12 05:26:20 +0300

    chnages in range

--- range.py
+++ range.py
@@ -8,3 +8,7 @@ print(in_range(10, 10, 10))
 # should print True
 print(in_range(5, 10, 20))
 # should print False
+#first number is the starting point, middle number is 
+# the ending point and the third is the range between numbers
+list1 = range(5, 15, 3) 
+list2 = range(0, 40, 5)
