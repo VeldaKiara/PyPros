ed1b9c97435397c04baa5daa93f4d70e675b7e02
Velda Kiara <veldakarimi@gmail.com>
2020-05-15 00:07:08 +0300

    Used len and index to call items on list

--- list.py
+++ list.py
@@ -11,6 +11,11 @@ name_and_age = zip(first_names, all_ages)
 #adding range
 ids = range(4)
 
+#2 is the starting point, 20 is the last number and the last 2 is the difference between the numbers expected
 list1 = range(2, 20, 3)
-list1_len = len(list1)
+list1_len = len(list1) #len is for the length of the list
 print(list1_len)
+
+employees = ['Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert']
+index4 =(employees[4]) #Index to call item number.
+print(len(employees)) #length of lists.
