ebd973f0f92db48efbf439773b90cbeffca531f4
Velda Kiara <veldakarimi@gmail.com>
2020-05-15 03:09:24 +0300

    selection of first item in the list

--- list.py
+++ list.py
@@ -84,4 +84,4 @@ print(games_sorted)
 
 inventory = ['twin bed', 'twin bed', 'headboard', 'queen bed', 'king bed', 'dresser', 'dresser', 'table', 'table', 'nightstand', 'nightstand', 'king bed', 'king bed', 'twin bed', 'twin bed', 'sheets', 'sheets', 'pillow', 'pillow']
 inventory_len = len(inventory)
-
+first = inventory[0]#selection of first element
