36f6837f79bd3f084f5f06fbd3cdb34c60a95bdc
Velda Kiara <veldakarimi@gmail.com>
2020-05-19 23:07:00 +0300

    addition of tuple file and some explanations

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#tuples are immutable, they cannot be changed.
+#tuples use ()
+#unpacking a tuple uses the number of info
+#an example of unpacking
+#my_deets = (Velda, 22, web dev)
+#how to unpack:
+#name,age, occupation = my_deets
+#name
+#prints Velda
+#tuples can also have trailing commas.
+#the order of tuples is important.
