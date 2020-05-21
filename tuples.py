a5f31bbc75acc9108826b61f1dae58501cf7f928
Velda Kiara <veldakarimi@gmail.com>
2020-05-21 23:57:26 +0300

    conversion of celsius to fahrenheit

--- tuples.py
+++ tuples.py
@@ -73,4 +73,5 @@ can_ride_coaster = [ height for height in heights if height > 161]
 print(can_ride_coaster)
     
 celsius = [0, 10, 15, 32, -5, 27, 3]
-
+fahrenheit = [temp*(9/5) + 32 for temp in celsius] #converting temp to fahrenheit from celsius
+print(fahrenheit)
