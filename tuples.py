9cb5f16d60ae6498578d842f16a319c424eef33a
Velda Kiara <veldakarimi@gmail.com>
2020-05-21 23:04:23 +0300

    addition of location

--- tuples.py
+++ tuples.py
@@ -61,4 +61,7 @@ while len(students_in_poetry) < 6:
 #nested loops
 sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
 scoops_sold = 0
-
+for location in sales_data:
+      print(location)
+      
+    
