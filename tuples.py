08e191284455e1deecb2c39008770f173921bf71
Velda Kiara <veldakarimi@gmail.com>
2020-05-21 23:28:03 +0300

    addition of list conversion

--- tuples.py
+++ tuples.py
@@ -68,4 +68,6 @@ for location in sales_data:
 
 #list compressions
 heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]   
+can_ride_coaster = [ height for height in heights if height > 161]
+print(can_ride_coaster)
     
