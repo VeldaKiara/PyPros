2395a05da15f71006b56553b5ebe95765ffbd73a
Velda Kiara <veldakarimi@gmail.com>
2020-05-21 23:14:08 +0300

    addition of  sum of scoops

--- tuples.py
+++ tuples.py
@@ -60,8 +60,10 @@ while len(students_in_poetry) < 6:
   
 #nested loops
 sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
-scoops_sold = 0
 for location in sales_data:
       print(location)
+      for scoops in location:
+        scoops_sold += scoops
+        print(scoops_sold)
       
     
