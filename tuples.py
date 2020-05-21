51762e683fb99e038a15959947c2ba8d7ef27b19
Velda Kiara <veldakarimi@gmail.com>
2020-05-22 00:10:13 +0300

    squaring of numbers

--- tuples.py
+++ tuples.py
@@ -79,8 +79,15 @@ print(fahrenheit)
 
 single_digits=[0,1,2,3,4,5,6,7,8,9]
 squares = []
-
 for single in single_digits:
-  print(single)
-  
+      print(single)
+      squares.append(single ** 2) #squares numbers
+      print(squares)
+      
+      
+      
+      
+ 
+
+
   
