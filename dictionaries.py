f4090ae57cc7bcc0bfebf77477bba9e13b625cd9
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 15:22:26 +0300

    using a dict with list comptehensions

--- dictionaries.py
+++ dictionaries.py
@@ -49,3 +49,6 @@ drinks = ["espresso", "chai", "decaf", "drip"]
 caffeine = [64, 40, 0, 120]
 
 zipped_drinks = zip(drinks, caffeine) 
+#creating a dict and using list comprehensions
+drinks_to_caffeine = {key:value for key, value in zipped_drinks}
+print(drinks_to_caffeine)
