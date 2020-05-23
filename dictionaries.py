ca24289574aa8b297967c22886f439e743576a44
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 13:51:22 +0300

    initial commit

--- dictionaries.py
+++ dictionaries.py
@@ -4,3 +4,8 @@
     Each key: value pair (i.e., "oatmeal": 3 or "avocado toast": 6) is separated by a comma (,)
     It’s considered good practice to insert a space () after each comma,
      but your code will still run without the space.'''
+     
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20}
+#num_cameras = {"backyard": 6  "garage": 2 "driveway": 1}
+
+print(sensors)
