ffb746849d396a6b1d3d54e090f4b5180252f8bf
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 19:02:52 +0300

    getting women percentage in occupations

--- dictionaries.py
+++ dictionaries.py
@@ -177,3 +177,5 @@ pct_women_in_occupation = {
     "Lawyer": 37, 
     "Aerospace Engineer": 9
     }
+for occupation, percentage in pct_women_in_occupation.items():
+  print("Women make up " + str(percentage) + " percent of " + occupation + "s.") 
