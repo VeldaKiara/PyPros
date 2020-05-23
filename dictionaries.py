e505fdffd883c010feff615f2eeaa3491a4000d1
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 15:29:30 +0300

    zipping, list comprehensions and dict

--- dictionaries.py
+++ dictionaries.py
@@ -56,3 +56,8 @@ print(drinks_to_caffeine)
 #music streaming
 songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
 playcounts = [78, 29, 44, 21, 89, 5]
+
+plays = {
+  key:value for key, value in zip(songs, playcounts)
+}
+print(plays)
