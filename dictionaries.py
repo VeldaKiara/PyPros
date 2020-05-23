8643b52d79c434e5b1c6d87ff118c9652f5e4604
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 13:53:47 +0300

    addition of values to a dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -7,5 +7,5 @@
      
 sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20}
 #num_cameras = {"backyard": 6  "garage": 2 "driveway": 1}
-
+sensors["pantry"] = 22 #addition to a dictionary
 print(sensors)
