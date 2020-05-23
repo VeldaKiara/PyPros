b83c2a4050c84b93e65944df773771120cbe2b63
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 13:36:05 +0300

    best practices of dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,6 @@
 #A dictionary is an unordered set of key: value pairs.
+#A dictionary begins and ends with curly braces ({ and }).
+''' Each item consists of a key (i.e., “oatmeal”) and a value (i.e., 3)
+    Each key: value pair (i.e., "oatmeal": 3 or "avocado toast": 6) is separated by a comma (,)
+    It’s considered good practice to insert a space () after each comma,
+     but your code will still run without the space.'''
