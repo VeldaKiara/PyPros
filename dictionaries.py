0dbe3a445047c78644cdd06ea27fb7c3ab566075
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 13:59:21 +0300

    changes to cameras

--- dictionaries.py
+++ dictionaries.py
@@ -6,6 +6,6 @@
      but your code will still run without the space.'''
      
 sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20}
-#num_cameras = {"backyard": 6  "garage": 2 "driveway": 1}
+num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
 sensors["pantry"] = 22 #addition to a dictionary
 print(sensors)
