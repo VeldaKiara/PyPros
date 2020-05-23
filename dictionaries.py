f23dee7868b31f958a46f062666bd4f5512aaf5e
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 16:07:53 +0300

    using user ids

--- dictionaries.py
+++ dictionaries.py
@@ -94,3 +94,10 @@ except KeyError:
   print("unknown Caffeine level")
 
 #using .get() method
+user_ids = {
+    "teraCoder": 100019, 
+    "pythonGuy": 182921, 
+    "samTheJavaMaam": 123112, 
+    "lyleLoop": 102931, 
+    "keysmithKeith": 129384
+    }
