582ac53d42b679f2b7d9a8609ed3a325262063aa
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 14:29:45 +0300

    adding oscar winners

--- dictionaries.py
+++ dictionaries.py
@@ -34,3 +34,6 @@ print(animals_in_zoo)
 user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
 user_ids.update({"theLooper": 138475, "stringQueen": 85739}) #adding more elements to dicts
 print(user_ids)
+
+#overwriting elemants
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
