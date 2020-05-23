2924389f87e405d5110d0fb0c3e0c91b805e0698
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 16:04:23 +0300

    added matcha

--- dictionaries.py
+++ dictionaries.py
@@ -86,7 +86,7 @@ print(zodiac_elements["energy"])
 
 #Try/Except to Get a Key
 caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120}
-
+caffeine_level["matcha"] = 30
 key_to_check = "matcha"
 try:
   print(caffeine_level[key_to_check])
