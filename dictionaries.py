2faf380da8084b43ffaa2c64140cefd4491b082d
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 14:05:12 +0300

    English to sindarin

--- dictionaries.py
+++ dictionaries.py
@@ -9,3 +9,12 @@ sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20}
 num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
 sensors["pantry"] = 22 #addition to a dictionary
 print(sensors)
+
+#translations from English to Sindarin
+translations = {
+  'mountain': 'orod',
+  'bread': 'bass',
+  'friend': 'mellon',
+  'horse': 'roch',
+
+}
