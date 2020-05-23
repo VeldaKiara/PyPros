f231e46729db02729bbd6860aff846e3723de877
Velda Kiara <veldakarimi@gmail.com>
2020-05-23 18:03:16 +0300

    popping stamina grains

--- dictionaries.py
+++ dictionaries.py
@@ -108,3 +108,13 @@ print(tc_id)
 print(stack_id)
 
 #deleting  a key .pop()
+available_items = {
+    "health potion": 10, 
+    "cake of the cure": 5, 
+    "green elixir": 20, 
+    "strength sandwich": 25, 
+    "stamina grains": 15, 
+    "power stew": 30}
+health_points = 20
+
+health_points += available_items.pop("stamina grains", 0)
