72eb06fd195511dd90ecc0f07546fb5be4db7a49
Velda Kiara <veldakarimi@gmail.com>
2020-06-02 01:31:45 +0300

    addition of instance of objects

--- class.py
+++ class.py
@@ -54,3 +54,17 @@ class Circle:
     
 teaching_table = Circle(36)
 print(teaching_table)
+
+#data held by an object is referred to as an instance variable.
+#The data held by an object is referred to as an instance variable.
+#  Instance variables aren’t shared by all instances of a class
+#  — they are variables 
+# that are specific to the object they are attached to.
+
+class Store:
+    pass
+alternative_rocks = Store()
+isabelles_ices = Store()
+
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
