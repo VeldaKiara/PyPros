405c96863d07c75071e1c0ef3ad6c1fee486fa13
Velda Kiara <veldakarimi@gmail.com>
2020-06-03 02:30:56 +0300

    addition of repr, string representation.

--- class.py
+++ class.py
@@ -130,3 +130,30 @@ print(dir(5))
 def this_function_is_an_object():
    return "happy"
 print(dir(this_function_is_an_object))
+
+#dunder method called __repr__. This is a method we can use to tell 
+# Python what we want the string representation of the class to be.
+#  __repr__ can only have one parameter, self, and must return a string.
+
+class Circle:
+    pi = 3.14
+  
+    def __init__(self, diameter):
+      self.radius = diameter / 2
+  
+    def area(self):
+      return self.pi * self.radius ** 2
+  
+    def circumference(self):
+      return self.pi * 2 * self.radius
+  
+    def __repr__(self):
+      return "Circle with radius {radius}".format(radius=self.radius)
+    
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print(medium_pizza)
+print(teaching_table)
+print(round_room)
