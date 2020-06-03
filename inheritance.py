be5864db39c48c4679522bcbaad8d71e8f1846e8
Velda Kiara <veldakarimi@gmail.com>
2020-06-03 03:37:21 +0300

    out of stock exception

--- inheritance.py
+++ inheritance.py
@@ -5,3 +5,31 @@ class Bin:
 
 class RecyclingBin(Bin):
   pass
+"""An Exception is a class that inherits from Python’s Exception class.
+issubclass() is a Python built-in function that takes two parameters.
+issubclass() returns True if the first argument is a subclass of the
+second argument. It returns False if the first class is not a 
+subclass of the second. issubclass() raises a TypeError if 
+either argument passed in is not a class."""
+
+issubclass(ZeroDivisionError, Exception)
+# Returns True because it is a subclass of Exception.
+
+#out of stock exception
+class OutOfStock(Exception): 
+      pass
+class CandleShop:
+  name = "Here's a Hot Tip: Buy Drip Candles"
+  def __init__(self, stock):
+    self.stock = stock
+    
+  def buy(self, color):
+    if self.stock[color] < 1:
+      raise OutOfStock
+    self.stock[color] = self.stock[color] - 1
+
+candle_shop = CandleShop({'blue': 6, 'red': 2, 'green': 0})
+candle_shop.buy('blue')
+
+# This should raise OutOfStock:
+candle_shop.buy('green')
